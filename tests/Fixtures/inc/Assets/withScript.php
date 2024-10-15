094231fca0fcd71b4bbabf8b0cbf100ad0933fa1
COQUARD Cyrille <coquardcyr@gmail.com>
2024-10-15 23:20:51 +0200

    Added new test to check that the custom key is used

--- tests/Fixtures/inc/Assets/withScript.php
+++ tests/Fixtures/inc/Assets/withScript.php
@@ -98,5 +98,29 @@ return [
 			'register' => true,
 			'return' => 'plugin_slugmy_key',
 		]
-	]
+	],
+	'enqueueShouldCallQueueFunction' => [
+		'config' => [
+			'plugin_url' => 'http://example.org/wp-content/plugin',
+			'enqueue' => true,
+			'register' => false,
+			'key' => 'my_key',
+			'url' => 'http://example.org/js/example.js',
+			'called' => true,
+			'content' => json_encode([
+				'app' => [
+					'js' => [
+						'dependency.js',
+						'app.hash.js',
+					]
+				]
+			])
+		],
+		'expected' => [
+			'entrypoints_path' => '/path/wp-content/plugin//assets/entrypoints.json',
+			'enqueue' => true,
+			'register' => false,
+			'return' => 'plugin_slugmy_key',
+		]
+	],
 ];
